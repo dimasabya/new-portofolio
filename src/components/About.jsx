fd56342776e5c2194af44a9023857a212b1ebe0c
dimasabya <dimasabdulyasir9@gmail.com>
2024-01-01 18:43:11 +0700

    margin

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -65,7 +65,7 @@ const About = () => {
               onClick={linkdin}
               data-aos="slide-up"
               data-aos-duration="1000"
-              className="px-2 py-1 md:p-2 mt-[5rem] md:mt-6 bg-black rounded-lg text-white font-semibold hover:text-red-400"
+              className="px-2 py-1 md:p-2 mt-[3rem] md:mt-6 bg-black rounded-lg text-white font-semibold hover:text-red-400"
             >
               Hire Me
             </button>
@@ -73,11 +73,11 @@ const About = () => {
               onClick={downloadCv}
               data-aos="slide-up"
               data-aos-duration="1000"
-              className="px-2 py-1 md:p-2 mt-[5rem] md:mt-6 bg-white rounded-lg text-black font-semibold hover:text-red-400 ml-4"
+              className="px-2 py-1 md:p-2 mt-[3rem] md:mt-6 bg-white rounded-lg text-black font-semibold hover:text-red-400 ml-4"
             >
               My CV <i className="fa-solid fa-download hover:text-red-400 delay-[.3s] ease-in"></i>
             </button>
-            <div data-aos="slide-up" data-aos-duration="1000" className="mt-5">
+            <div data-aos="slide-up" data-aos-duration="1000" className="mt-3">
               <i className="fa-brands fa-square-instagram text-2xl md:text-3xl mx-2  hover:scale-125"></i>
               <i className="fa-brands fa-square-facebook text-2xl md:text-3xl mx-2  hover:scale-125"></i>
               <i className="fa-brands fa-square-twitter text-2xl md:text-3xl mx-2  hover:scale-125"></i>
